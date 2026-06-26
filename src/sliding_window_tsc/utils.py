6a8762d8d11e95aed14936c9d66ca5902706c026
Gabriele Lorenzo Singe <gabrielelorenzo.singe@mail.polimi.it>
2026-06-26 12:36:43 +0200

    Add ideal and fast classifier name lists
    Introduce src/sliding_window_tsc/utils.py defining two lists of classifier names: IDEAL_CLASSIFIERS and FAST_TRAINING_CLASSIFIERS. These lists centralize model names used for sliding-window time series classification experiments to simplify classifier selection and benchmarking.

---
+++ src/sliding_window_tsc/utils.py
@@ -0,0 +1,18 @@
+IDEAL_CLASSIFIERS = [
+    "MiniRocketClassifier",
+    "KNeighborsTimeSeriesClassifier",
+    "WEASEL",
+    "Catch22Classifier",
+    "DrCIFClassifier",
+    "RDSTClassifier",
+    "InceptionTimeClassifier",
+]
+
+FAST_TRAINING_CLASSIFIERS = [
+    "MiniRocketClassifier",
+    "SummaryClassifier",
+    "Catch22Classifier",
+    "TimeSeriesForestClassifier",
+    "RandomIntervalClassifier",
+    "KNeighborsTimeSeriesClassifier",
+]
