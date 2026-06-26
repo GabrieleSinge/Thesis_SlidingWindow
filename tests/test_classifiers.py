418e5ecf2f121472982042a05ca3040d629044d1
Gabriele Lorenzo Singe <gabrielelorenzo.singe@mail.polimi.it>
2026-06-26 10:29:09 +0200

    Add tests for get_classifier_by_name
    Add pytest unit tests for get_classifier_by_name

---
+++ tests/test_classifiers.py
@@ -0,0 +1,15 @@
+import pytest
+
+from classifiers import get_classifier_by_name
+
+
+def test_get_classifier_by_name_valid_classifier():
+    clf = get_classifier_by_name("MiniRocketClassifier")
+
+    assert clf is not None
+    assert clf.__class__.__name__ == "MiniRocketClassifier"
+
+
+def test_get_classifier_by_name_invalid_classifier():
+    with pytest.raises(ValueError):
+        get_classifier_by_name("ClassifierCheNonEsiste")
