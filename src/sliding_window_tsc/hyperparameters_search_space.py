449b8f6352d563d94c63a70652679cb48d7d2360
Gabriele Lorenzo Singe <gabrielelorenzo.singe@mail.polimi.it>
2026-07-08 15:12:58 +0200

    Support InceptionTime & TSForest hyperparams
    Add hyperparameter suggestion blocks for InceptionTimeClassifier and TimeSeriesForestClassifier in suggest_hyperparameters.

--- src/sliding_window_tsc/hyperparameters_search_space.py
+++ src/sliding_window_tsc/hyperparameters_search_space.py
@@ -101,6 +101,76 @@ def suggest_hyperparameters(trial, classifier_name: str) -> dict:
             "support_probabilities": False,
             "n_jobs": 4,
         }
+    if classifier_name == "InceptionTimeClassifier":
+        return {
+            "n_classifiers": trial.suggest_categorical(
+                "n_classifiers",
+                [1, 3, 5],
+            ),
+            "n_filters": trial.suggest_categorical(
+                "n_filters",
+                [16, 32, 64],
+            ),
+            "kernel_size": trial.suggest_categorical(
+                "kernel_size",
+                [20, 40, 60],
+            ),
+            "depth": trial.suggest_categorical(
+                "depth",
+                [4, 6, 8],
+            ),
+            "n_conv_per_layer": trial.suggest_categorical(
+                "n_conv_per_layer",
+                [2, 3],
+            ),
+            "bottleneck_size": trial.suggest_categorical(
+                "bottleneck_size",
+                [16, 32, 64],
+            ),
+            "use_residual": trial.suggest_categorical(
+                "use_residual",
+                [True, False],
+            ),
+            "use_bottleneck": trial.suggest_categorical(
+                "use_bottleneck",
+                [True, False],
+            ),
+            "use_max_pooling": trial.suggest_categorical(
+                "use_max_pooling",
+                [True, False],
+            ),
+            "batch_size": trial.suggest_categorical(
+                "batch_size",
+                [16, 32, 64],
+            ),
+            "n_epochs": trial.suggest_categorical(
+                "n_epochs",
+                [50, 100, 150],
+            ),
+            "verbose": False,
+            "random_state": 42,
+            "save_last_model": False,
+            "save_best_model": False,
+            "save_init_model": False,
+        }
+    
+    if classifier_name == "TimeSeriesForestClassifier":
+        return {
+            "n_estimators": trial.suggest_categorical(
+                "n_estimators",
+                [100, 200, 300],
+            ),
+            "n_intervals": trial.suggest_categorical(
+                "n_intervals",
+                ["sqrt", "log", 20, 50, 100],
+            ),
+            "min_interval_length": trial.suggest_categorical(
+                "min_interval_length",
+                [3, 5, 10],
+            ),
+            "n_jobs": 4,
+            "random_state": 42,
+        }
 
     if classifier_name in {"Catch22Classifier", "SummaryClassifier"}:
         return {}
